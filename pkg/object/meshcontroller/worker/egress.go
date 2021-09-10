6cfc04a8966756075a1ada156176c4ab8a52eeaa
benjaminwu <benjamin2013elka@gmail.com>
2021-09-10 20:48:01 +0800

    [mesh] sidecar support egress host matching

--- pkg/object/meshcontroller/worker/egress.go
+++ pkg/object/meshcontroller/worker/egress.go
@@ -165,6 +165,14 @@ func (egs *EgressServer) reloadBySpecs(value map[string]*spec.Service) bool {
 	return egs.reloadHTTPServer(value)
 }
 
+// regex rule: ^(|(\w+\.)+)vet-services\.(\w+)\.svc\..+$
+//  can match e.g. _tcp.vet-services.easemesh.svc.cluster.local
+//   		   vet-services.easemesh.svc.cluster.local
+//   		   _zip._tcp.vet-services.easemesh.svc.com
+func (egs *EgressServer) buildHostRegex(serviceName string) string {
+	return fmt.Sprintf("^(|(\\w+\\.)+)%s\\.(\\w+)\\.svc\\..+", serviceName)
+}
+
 func (egs *EgressServer) reloadHTTPServer(specs map[string]*spec.Service) bool {
 	egs.mutex.Lock()
 	defer egs.mutex.Unlock()
@@ -191,7 +199,8 @@ func (egs *EgressServer) reloadHTTPServer(specs map[string]*spec.Service) bool {
 	httpServerSpec := egs.httpServer.Spec().ObjectSpec().(*httpserver.Spec)
 	httpServerSpec.Rules = nil
 
-	for k := range pipelines {
+	for serviceName := range pipelines {
+		// for matching agent enhanceable request
 		rule := &httpserver.Rule{
 			Paths: []*httpserver.Path{
 				{
@@ -200,17 +209,29 @@ func (egs *EgressServer) reloadHTTPServer(specs map[string]*spec.Service) bool {
 						{
 							Key: egressRPCKey,
 							// Value should be the service name
-							Values:  []string{k},
-							Backend: serverName2PipelineName[k],
+							Values:  []string{serviceName},
+							Backend: serverName2PipelineName[serviceName],
 						},
 					},
 					// this name should be the pipeline full name
-					Backend: serverName2PipelineName[k],
+					Backend: serverName2PipelineName[serviceName],
 				},
 			},
 		}
 
-		httpServerSpec.Rules = append(httpServerSpec.Rules, rule)
+		// for matching only host name request, need to matching by host name with regexp
+		ruleHost := &httpserver.Rule{
+			HostRegexp: egs.buildHostRegex(serviceName),
+			Paths: []*httpserver.Path{
+				{
+					PathPrefix: "/",
+					// this name should be the pipeline full name
+					Backend: serverName2PipelineName[serviceName],
+				},
+			},
+		}
+
+		httpServerSpec.Rules = append(httpServerSpec.Rules, rule, ruleHost)
 	}
 
 	builder := newHTTPServerSpecBuilder(egs.egressServerName, httpServerSpec)
